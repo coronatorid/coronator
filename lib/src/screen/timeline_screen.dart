38dae243445d51ccb5db1b4cfdc9283ad79f2065
insomnius <aliana.arib@gmail.com>
2020-11-05 06:40:47 +0700

    URL launcher

--- lib/src/screen/timeline_screen.dart
+++ lib/src/screen/timeline_screen.dart
@@ -1,5 +1,9 @@
 import 'package:coronator/src/core/color.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TimelineScreen extends StatelessWidget {
   @override
@@ -21,11 +25,47 @@ class TimelineScreen extends StatelessWidget {
               SizedBox(
                 height: 10,
               ),
-              Text(
-                "Log aktifitas Anda.",
-                style: TextStyle(
-                  fontFamily: 'Hind',
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Log aktifitas Anda.",
+                    style: TextStyle(
+                      fontFamily: 'Hind',
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      const phoneNumber = 'tel:112';
+                      if (await canLaunch(phoneNumber)) {
+                        await launch(phoneNumber);
+                      } else {}
+                    },
+                    child: Container(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.phone,
+                            size: 15,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Hubungi Call Center 112",
+                            style: TextStyle(
+                              fontFamily: 'Hind',
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
               Divider(),
               Expanded(
@@ -46,7 +86,6 @@ class TimelineScreen extends StatelessWidget {
 
   Widget suspectedCases(BuildContext context) {
     return Container(
-      // padding: EdgeInsets.symmetric(vertical: 12.5, horizontal: 15),
       child: Row(
         children: [
           // https://www.flaticon.com/free-icon/sick_983053?term=sick&page=1&position=69&k=1604432100038
@@ -104,6 +143,25 @@ class TimelineScreen extends StatelessWidget {
                                 decoration: TextDecoration.underline,
                                 color: Colors.blue,
                               ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () async {
+                                  if (await canLaunch(
+                                      "https://www.google.com/maps/@-6.2828349,106.8194158,20z")) {
+                                    await launch(
+                                      "https://www.google.com/maps/@-6.2828349,106.8194158,20z",
+                                    );
+                                  } else {
+                                    Clipboard.setData(
+                                      ClipboardData(
+                                          text:
+                                              "https://www.google.com/maps/@-6.2828349,106.8194158,20z"),
+                                    );
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                      content: Text(
+                                          "URL berhasil di copy, silahkan buka di browser karena aplikasi tidak bisa membukanya langsung."),
+                                    ));
+                                  }
+                                },
                             )
                           ],
                         ),
@@ -173,12 +231,31 @@ class TimelineScreen extends StatelessWidget {
                               text: " corona!",
                             ),
                             TextSpan(
-                              text: "\n\nLihat lokasi disini",
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                color: Colors.blue,
-                              ),
-                            )
+                                text: "\n\nLihat lokasi disini",
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.blue,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () async {
+                                    if (await canLaunch(
+                                        "https://www.google.com/maps/@-6.2828349,106.8194158,20z")) {
+                                      await launch(
+                                        "https://www.google.com/maps/@-6.2828349,106.8194158,20z",
+                                      );
+                                    } else {
+                                      Clipboard.setData(
+                                        ClipboardData(
+                                            text:
+                                                "https://www.google.com/maps/@-6.2828349,106.8194158,20z"),
+                                      );
+                                      Scaffold.of(context)
+                                          .showSnackBar(SnackBar(
+                                        content: Text(
+                                            "URL berhasil di copy, silahkan buka di browser karena aplikasi tidak bisa membukanya langsung."),
+                                      ));
+                                    }
+                                  })
                           ],
                         ),
                       ),
