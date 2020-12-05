0f51081538361817f1fb1b150dd64659bdc218e4
insomnius <aliana.arib@gmail.com>
2020-12-05 13:53:22 +0700

    Send otp click guard

--- lib/src/controller/login_controller.dart
+++ lib/src/controller/login_controller.dart
@@ -4,8 +4,12 @@ import 'package:coronator/src/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:synchronized/synchronized.dart';
 
 class LoginController implements LoginInterface {
+  Lock _sendOtpLock = Lock();
+  bool _sendOtpClicked = false;
+
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => LoginProvider(""),
@@ -14,22 +18,31 @@ class LoginController implements LoginInterface {
   }
 
   void sendOtp(BuildContext context) {
-    LoginProvider loginProvider =
-        Provider.of<LoginProvider>(context, listen: false);
+    Scaffold.of(context).hideCurrentSnackBar();
 
-    RegExp regExp = new RegExp(
-      r"^\+62\d{10,12}",
-      caseSensitive: false,
-      multiLine: false,
-    );
+    this._sendOtpLock.synchronized(() {
+      if (this._sendOtpClicked) return;
 
-    if (regExp.hasMatch(loginProvider.phoneNumber()) == false) {
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Nomor telfon tidak valid"),
-      ));
-      return;
-    }
+      try {
+        LoginProvider loginProvider =
+            Provider.of<LoginProvider>(context, listen: false);
 
-    Navigator.of(context).pushNamed('/otp');
+        RegExp regExp = new RegExp(
+          r"^\+62\d{10,12}",
+          caseSensitive: false,
+          multiLine: false,
+        );
+
+        if (regExp.hasMatch(loginProvider.phoneNumber()) == false) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text("Nomor telfon tidak valid"),
+          ));
+          return;
+        }
+
+        loginProvider.setPhoneNumber("");
+        Navigator.of(context).pushNamed('/otp');
+      } catch (e) {}
+    });
   }
 }
