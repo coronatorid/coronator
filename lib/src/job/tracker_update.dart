316eb88865b7c1ceb1fe64dea7359207ff53299b
insomnius <aliana.arib@gmail.com>
2020-12-16 00:43:06 +0700

    Check permission in location tracker background job

--- lib/src/job/tracker_update.dart
+++ lib/src/job/tracker_update.dart
@@ -1,5 +1,6 @@
 import 'package:coronator/src/core/api.dart';
 import 'package:coronator/src/provider/auth_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class TrackerUpdate {
   final AuthProvider authProvider;
@@ -9,6 +10,11 @@ class TrackerUpdate {
 
   Future<void> perform() async {
     if (authProvider.isLogin() == false) {
+      return;
+    }
+
+    if (await Permission.locationAlways.status.isDenied) {
+      print("LOCATION UPDATE FAILED BECAUSE LOCATION STATUS DENIED");
       return;
     }
 
