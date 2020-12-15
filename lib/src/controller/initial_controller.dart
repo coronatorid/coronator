25f422dfb16f7117adbbba415b8184ff48cce535
insomnius <aliana.arib@gmail.com>
2020-12-15 23:52:22 +0700

    Permission manager

--- lib/src/controller/initial_controller.dart
+++ lib/src/controller/initial_controller.dart
@@ -2,6 +2,7 @@ import 'package:coronator/src/controller/timeline_controller.dart';
 import 'package:coronator/src/provider/auth_provider.dart';
 import 'package:coronator/src/screen/initial_screen.dart';
 import 'package:flutter/widgets.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 class InitialController {
@@ -14,6 +15,15 @@ class InitialController {
     if (authProvider.isLogin()) {
       return TimelineController().build(context);
     }
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      PermissionStatus status = await Permission.locationAlways.request();
+      if (status.isDenied) {
+        print("LOCATION PERMISSION DENIED :'(");
+      } else {
+        print("LOCATION PERMISSION ACCEPTED");
+      }
+    });
 
     return InitialScreen();
   }
