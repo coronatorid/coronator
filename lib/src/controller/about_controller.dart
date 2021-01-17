bdccdc24d970e5a9ec31f71f32f07f2c92491668
insomnius <aliana.arib@gmail.com>
2021-01-18 00:16:41 +0700

    About screen

--- lib/src/controller/about_controller.dart
+++ lib/src/controller/about_controller.dart
@@ -1,8 +1,16 @@
+import 'package:coronator/src/provider/about_provider.dart';
 import 'package:coronator/src/screen/about_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AboutController {
   Widget build(BuildContext context) {
-    return AboutScreen();
+    AboutProvider aboutProvider = AboutProvider();
+    aboutProvider.init();
+
+    return ChangeNotifierProvider(
+      create: (_) => aboutProvider,
+      child: AboutScreen(),
+    );
   }
 }
