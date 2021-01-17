b1fb69f2c02f68afb60e2084208bb16fecded3fe
insomnius <aliana.arib@gmail.com>
2021-01-17 23:29:56 +0700

    Add title text to settings page

--- lib/src/screen/setting_screen.dart
+++ lib/src/screen/setting_screen.dart
@@ -14,100 +14,124 @@ class SettingScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: ListView(
+          child: Column(
             children: [
-              SizedBox(
-                height: 7.5,
-              ),
-              InkWell(
-                onTap: () async {
-                  if (await canLaunch('https://coronator.id/terms')) {
-                    await launch(
-                      'https://coronator.id/terms',
-                    );
-                  } else {
-                    Clipboard.setData(
-                      ClipboardData(text: 'https://coronator.id/terms'),
-                    );
-
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                        "URL Terms berhasil dicopy. Silahkan buka di browser.",
-                      ),
-                    ));
-                  }
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 20,
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Syarat & Ketentuan",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontFamily: "Hind",
-                        ),
-                      ),
-                    ],
-                  ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 20,
                 ),
-              ),
-              Divider(),
-              InkWell(
-                onTap: () async {
-                  if (await canLaunch('https://coronator.id/privacy')) {
-                    await launch(
-                      'https://coronator.id/privacy',
-                    );
-                  } else {
-                    Clipboard.setData(
-                      ClipboardData(text: 'https://coronator.id/privacy'),
-                    );
-
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                        "URL Terms berhasil dicopy. Silahkan buka di browser.",
+                child: Row(
+                  children: [
+                    Text(
+                      "Settings",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
                       ),
-                    ));
-                  }
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 20,
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Privasi",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontFamily: "Hind",
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Button(
-                      onTap: (BuildContext context) async {
-                        await this.interface.tapLogout(context);
-                      },
-                      buttonText: "LOG OUT",
-                      width: 175,
                     ),
-                  ),
-                ],
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    SizedBox(
+                      height: 15,
+                    ),
+                    InkWell(
+                      onTap: () async {
+                        if (await canLaunch('https://coronator.id/terms')) {
+                          await launch(
+                            'https://coronator.id/terms',
+                          );
+                        } else {
+                          Clipboard.setData(
+                            ClipboardData(text: 'https://coronator.id/terms'),
+                          );
+
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                              "URL Terms berhasil dicopy. Silahkan buka di browser.",
+                            ),
+                          ));
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Syarat & Ketentuan",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontFamily: "Hind",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    InkWell(
+                      onTap: () async {
+                        if (await canLaunch('https://coronator.id/privacy')) {
+                          await launch(
+                            'https://coronator.id/privacy',
+                          );
+                        } else {
+                          Clipboard.setData(
+                            ClipboardData(text: 'https://coronator.id/privacy'),
+                          );
+
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                              "URL Terms berhasil dicopy. Silahkan buka di browser.",
+                            ),
+                          ));
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Privasi",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontFamily: "Hind",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Button(
+                            onTap: (BuildContext context) async {
+                              await this.interface.tapLogout(context);
+                            },
+                            buttonText: "LOG OUT",
+                            width: 175,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
